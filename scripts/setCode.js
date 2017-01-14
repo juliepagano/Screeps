0cd4590985f83421d2b4b0c7e833e2690a72093c
Julie Pagano <julie.pagano@gmail.com>
2017-01-14 13:51:17 -0800

    Add ability to force setting code when dirty branch.

--- scripts/setCode.js
+++ scripts/setCode.js
@@ -4,14 +4,21 @@ const request = require('superagent')
 const prompt = require('prompt')
 const child_process = require('child_process')
 const fs = require('fs')
+const argv = require('yargs')
+  .option('f', {
+    description: 'if specified, will ignore git status check and force update'
+  })
+  .argv
 
 console.log('Set Screeps code!')
+
+const force = !!argv.f
 
 try {
   const config = require('../config.json')
 
   // Use local config file for auth.
-  setData(config.username, config.password)
+  setData(config.username, config.password, force)
 } catch (e) {
   console.log(e)
 
@@ -37,12 +44,12 @@ try {
       console.log('Invalid input')
       console.error(err)
     } else {
-      setData()(result.username, result.password)
+      setData()(result.username, result.password, force)
     }
   })
 }
 
-function setData (username, password) {
+function setData (username, password, force) {
   const modules = readModules()
   const currentBranch = getCurrentBranch()
   const screepsBranch = currentBranch === 'master' ? 'default' : currentBranch
@@ -52,7 +59,7 @@ function setData (username, password) {
     modules: modules
   }
 
-  if (!checkForDirtyBranch()) {
+  if (!checkForDirtyBranch(force)) {
     console.log('')
     console.log(`sending data to branch ${screepsBranch}...`)
     request
@@ -73,21 +80,27 @@ function setData (username, password) {
   }
 }
 
-function getCurrentBranch() {
+function getCurrentBranch(force) {
   let currentBranch = child_process.execSync('git symbolic-ref HEAD')
     .toString()
   currentBranch = currentBranch.trim().match(/[A-Za-z0-9_\-]+$/)[0]
   return currentBranch
 }
 
-function checkForDirtyBranch () {
+function checkForDirtyBranch (force) {
   let dirtyBranch = child_process.execSync('git status --porcelain')
       .toString()
   dirtyBranch = dirtyBranch.length > 0
 
   if (dirtyBranch) {
     console.log('You have unsaved changes in your branch.')
-    console.log('Commit or stash your changes before updating from screeps.')
+
+    if (force) {
+      console.log('You have specified -f, so updating anyway.')
+      dirtyBranch = false
+    } else {
+      console.log('Commit or stash your changes before updating from screeps.')
+    }
   }
 
   return dirtyBranch
